6d14698bfc47b9e3805284a4858d79c0da5318a7
jiusong <jiusong@ebay.com>
2018-03-19 19:02:25 +0800

    updated.

--- esna/src/index.js
+++ esna/src/index.js
@@ -3,8 +3,31 @@ import ReactDom from 'react-dom'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {
+	BrowserRouter, 
+	Route, 
+	Link, 
+	Redirect, 
+	Switch
+} from 'react-router-dom'
+
 import App from './App'
 import {counter} from './index.redux'
+
+function Two(){
+	return <h2>two</h2>
+}
+function Three(){
+	return <h2>three</h2>
+}
+class Test extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return <h2>TEST {this.props.match.params.location}</h2>
+	}
+}
 
 const store = createStore(counter, compose(
 	applyMiddleware(thunk),
@@ -13,8 +36,31 @@ const store = createStore(counter, compose(
 
 ReactDom.render(
 	(<Provider store={store}>
-		<App />	
+		<BrowserRouter>
+			<div>
+				<ul>
+					<li>
+						<Link to='/'>one</Link>
+					</li>
+					<li>
+						<Link to='/two'>two</Link>
+					</li>
+					<li>
+						<Link to='/three'>three</Link>
+					</li>
+					<li>
+						<Link to='/123'>123</Link>
+					</li>
+				</ul>
+				<Switch>
+					{/*只渲染第一个匹配的Route*/}
+					<Route path='/' exact component={App}></Route>
+					<Route path='/two' component={Two}></Route>
+					<Route path='/three' component={Three}></Route>
+					<Redirect from='/123' to='/three'></Redirect>
+				</Switch>
+			</div>
+		</BrowserRouter>
 	</Provider>),
 	document.getElementById('root')
 )
-
