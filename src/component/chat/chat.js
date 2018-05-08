3afc96e35478dc95f7688bdcf61153739cdc0ad0
jiusong <jiusong@ebay.com>
2018-05-08 17:42:27 +0800

    updated.

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -6,6 +6,10 @@ import {getMsgList, sendMsg, recvMsg, listenerSet, readMsg, saveDraftMsg} from '
 import {getChatId} from '../../util'
 import QueueAnim from 'rc-queue-anim'
 
+//SSR中跳过对iScroll的设置, 因为会出现window not defined的错误;
+const ReactIScroll = process?null:require('react-iscroll')
+const iScroll = process?null:require('iscroll')
+
 @connect(
 	state=>state,
 	{getMsgList, sendMsg, recvMsg, listenerSet, readMsg, saveDraftMsg}
@@ -17,7 +21,8 @@ class Chat extends React.Component{
 			text:'',
 			showEmoji:false
 		}
-
+		this.chatmsgsLength = 0 //此聊天页面中所有需要显示的聊天记录的条数, 用来计算重新调整页面滚动条的等待时间;
+		this.alreadyUpdated = false
 		const msgDraft = this.props.chat.chatdraft[this.props.match.params.user]
 		if(msgDraft)
 			this.state.text = msgDraft
@@ -29,10 +34,9 @@ class Chat extends React.Component{
 			this.props.listenerSet()
 		}
 		setTimeout(()=>{
-			// document.documentElement.scrollTop = 10000 //for chrome
-			// document.getElementsByTagName("body")[0].scrollTop = 10000 //for safari
-			document.getElementsByClassName('chatContent')[0].scrollTop = 10000 //for both chrome&safari
-		},1500)
+			if(document.getElementsByClassName('chatContent')[0])
+				document.getElementsByClassName('chatContent')[0].scrollTop = 10000 //for both chrome&safari
+		},this.chatmsgsLength*100)
 	}
 	componentWillUnmount(){
 		const to = this.props.match.params.user
@@ -42,11 +46,19 @@ class Chat extends React.Component{
 		this.props.saveDraftMsg(to, chatDraft)
 	}
 	componentDidUpdate(){
-		setTimeout(()=>{
-			// document.documentElement.scrollTop = 10000 //for chrome
-			// document.getElementsByTagName("body")[0].scrollTop = 10000 //for safari
-			document.getElementsByClassName('chatContent')[0].scrollTop = 10000 //for both chrome&safari
-		},200)
+		//如果是首次带有聊天数据的update(this.chatmsgsLength>0), 那么要等待所有聊天消息显示完成后再调整滚动条, 如果是接收新消息的更新则等待时间固定;
+		if(this.alreadyUpdated){
+			setTimeout(()=>{
+				if(document.getElementsByClassName('chatContent')[0])
+					document.getElementsByClassName('chatContent')[0].scrollTop = 10000 //for both chrome&safari
+			},200)
+		}else if(this.chatmsgsLength>0){
+			setTimeout(()=>{
+				if(document.getElementsByClassName('chatContent')[0])
+					document.getElementsByClassName('chatContent')[0].scrollTop = 10000 //for both chrome&safari
+			},this.chatmsgsLength*100)
+			this.alreadyUpdated = true
+		}
 	}
 	whenFocusOnInput(){
 		setTimeout(()=>{
@@ -83,6 +95,7 @@ class Chat extends React.Component{
 
 		const chatid = getChatId(userid, this.props.user._id)
 		const chatmsgs = this.props.chat.chatmsg.filter(v=>v.chatid == chatid)
+		this.chatmsgsLength = chatmsgs.length
 		const redirect = this.props.user.redirectTo
 		return (
 			
@@ -98,30 +111,58 @@ class Chat extends React.Component{
 				>
 					{users[userid].name}
 				</NavBar>
-				<QueueAnim className='chatContent' type='scale' delay={100}>
-					{chatmsgs.map(v=>{
-						const avatar = require(`../img/${users[v.from].avatar}.png`)
-						return v.from == userid?(
-							<List key={v._id}>
-								<Item
-									thumb={avatar}
-									className='chat-who'
-									wrap
-									style={{wordWrap:'break-word'}}
-								>{v.content}</Item>
-							</List>
-						):(
-							<List key={v._id}>
-								<Item 
-									extra={<img src={avatar} alt=''/>}
-									className='chat-me'
-									wrap
-									style={{wordWrap:'break-word'}}
-								>{v.content}</Item>
-							</List>
-						)
-					})}
-				</QueueAnim>
+				{process?
+					<QueueAnim className='chatContent' type='scale' delay={100}>
+							{chatmsgs.map(v=>{
+								const avatar = require(`../img/${users[v.from].avatar}.png`)
+								return v.from == userid?(
+									<List key={v._id}>
+										<Item
+											thumb={avatar}
+											className='chat-who'
+											wrap
+											style={{wordWrap:'break-word'}}
+										>{v.content}</Item>
+									</List>
+								):(
+									<List key={v._id}>
+										<Item 
+											extra={<img src={avatar} alt=''/>}
+											className='chat-me'
+											wrap
+											style={{wordWrap:'break-word'}}
+										>{v.content}</Item>
+									</List>
+								)
+							})}
+					</QueueAnim>:
+					<ReactIScroll iScroll={iScroll}>
+						<QueueAnim className='chatContent' type='scale' delay={100}>
+								{chatmsgs.map(v=>{
+									const avatar = require(`../img/${users[v.from].avatar}.png`)
+									return v.from == userid?(
+										<List key={v._id}>
+											<Item
+												thumb={avatar}
+												className='chat-who'
+												wrap
+												style={{wordWrap:'break-word'}}
+											>{v.content}</Item>
+										</List>
+									):(
+										<List key={v._id}>
+											<Item 
+												extra={<img src={avatar} alt=''/>}
+												className='chat-me'
+												wrap
+												style={{wordWrap:'break-word'}}
+											>{v.content}</Item>
+										</List>
+									)
+								})}
+						</QueueAnim>
+					</ReactIScroll>
+				}
 				<div className='stick-footer'>
 					<List>
 						<InputItem
