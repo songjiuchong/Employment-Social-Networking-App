6410325ed1e7876ac8cc9635c69966398baa47c0
jiusong <jiusong@ebay.com>
2018-05-17 14:28:54 +0800

    updated.

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -15,24 +15,34 @@ class Register extends React.Component{
 	constructor(props){
 		super(props)
 		this.refEle = null
+		this.hasFocused = false //经过交互操作后最终页面稳定状态下是否有输入框处于focus状态, 这个属性将决定下次有输入框被focus时是否会执行位置调整的操作;
+		this.inFocus = false  //在onBlur事件触发后会被立即置false, 并且等待300ms后再判断是否有onFoucs事件再次将其置true, 如果有则不执行onBlur相关位置调整, 如果没有则执行位置调整并将hasFocused状态置false;
 		this.handleRegister = this.handleRegister.bind(this)
 		this.getRefEle = this.getRefEle.bind(this)
 		this.whenFocusOnInput = this.whenFocusOnInput.bind(this)
 		this.whenBlurOnInput = this.whenBlurOnInput.bind(this)
 	}
 	whenFocusOnInput(){
-		document.documentElement.scrollTop = 10000 //for chrome
-		document.getElementsByTagName("body")[0].scrollTop = 10000 //for safari
-		setTimeout(()=>{
-			this.refEle.style.position = 'relative'
-			this.refEle.style.bottom = '263px'
-		},200)
+		if(!this.hasFocused){
+			document.documentElement.scrollTop = 10000 //for chrome
+			document.getElementsByTagName("body")[0].scrollTop = 10000 //for safari
+			setTimeout(()=>{
+				this.refEle.style.position = 'relative'
+				this.refEle.style.bottom = '267px'
+				this.inFocus = true
+			},200)
+			this.hasFocused = true
+		}
 	}
 	whenBlurOnInput(){
+		this.inFocus  = false
 		setTimeout(()=>{
-			this.refEle.style.position = 'block'
-			this.refEle.style.bottom = '0'
-		},200)
+			if(!this.inFocus){
+				this.refEle.style.position = 'block'
+				this.refEle.style.bottom = '0'
+				this.hasFocused = false
+			}
+		},300)
 	}
 	getRefEle(ref){
 		if(ref){
